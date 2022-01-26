497526bf3a48bb90798d84e23bb17d0a5b2a97a1
unknown <hossainahmed359@gmail.com>
2022-01-26 12:34:51 +0600

    contact section complete

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppAbout from '../components/home/about';
+import AppContact from '../components/home/contact';
 import AppFaq from '../components/home/faq';
 import AppFeature from '../components/home/feature';
 import AppHero from '../components/home/hero';
@@ -15,6 +16,7 @@ const AppHome = () => {
             <AppWorks/>
             <AppFaq/>
             <AppPricing/>
+            <AppContact/>
         </div>
     );
 };
